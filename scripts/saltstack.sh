ddc261a3ae506782c4b7dbbac51a5bc324747f30
caangeenbrug-fvt <c.aangeenbrug@flikweertvision.nl>
2025-10-28 14:00:42 +0100

    Add sorting configuration topology to salt stack

--- scripts/saltstack.sh
+++ scripts/saltstack.sh
@@ -3,18 +3,23 @@ set -euo pipefail
 
 CMD="$(basename "$0")"
 COMPOSE=(docker compose)
+MINIONS=(salt-minion-panelpc salt-minion-qg-1 salt-minion-qg-2)
+ALL_CONTAINERS=(salt-master "${MINIONS[@]}")
 
 usage() {
   cat <<EOF
 Usage: $CMD <command> [args...]
 
 Commands:
-  start          Start the Salt master and all minion containers
-  stop           Stop the Salt containers
-  status         Show the Salt containers status
-  state [TARGET] [ARGS]
-                 Run salt <target> state.apply (default target: 'minion-*')
-  shell [CMD]    Open a shell (default: bash) in the salt-master container
+  start                 Start the Salt master and all minion containers
+  stop                  Stop the Salt containers
+  status                Show the Salt containers status
+  state [TARGET] [STATE] [ARGS]
+                        Run salt <target> state.apply <state> (defaults: target='minion-*', state='demo')
+  highstate [TARGET]    Run salt <target> state.highstate (default target='minion-*')
+  sync [TARGET]         Run salt <target> saltutil.sync_all to refresh modules/files (default target='minion-*')
+  pillar [TARGET] [KEY] Show pillar data (defaults: target='minion-*', command=pillar.items)
+  shell [CMD]           Open a shell (default: bash) in the salt-master container
 EOF
 }
 
@@ -28,16 +33,56 @@ shift
 
 case "$command" in
   start)
-    "${COMPOSE[@]}" up -d salt-master salt-minion-panelpc salt-minion-qg-1 salt-minion-qg-2
+    "${COMPOSE[@]}" up -d "${ALL_CONTAINERS[@]}"
     ;;
   stop)
-    "${COMPOSE[@]}" stop salt-master salt-minion-panelpc salt-minion-qg-1 salt-minion-qg-2
+    "${COMPOSE[@]}" stop "${ALL_CONTAINERS[@]}"
     ;;
   status)
-    "${COMPOSE[@]}" ps salt-master salt-minion-panelpc salt-minion-qg-1 salt-minion-qg-2
+    "${COMPOSE[@]}" ps "${ALL_CONTAINERS[@]}"
     ;;
   state)
-    "${COMPOSE[@]}" exec salt-master salt "*" state.apply
+    target="minion-*"
+    if [[ $# -gt 0 && "$1" != -* ]]; then
+      target="$1"
+      shift
+    fi
+    state_name="demo"
+    if [[ $# -gt 0 && "$1" != -* ]]; then
+      state_name="$1"
+      shift
+    fi
+    "${COMPOSE[@]}" exec salt-master salt "$target" state.apply "$state_name" "$@"
+    ;;
+  highstate)
+    target="minion-*"
+    if [[ $# -gt 0 && "$1" != -* ]]; then
+      target="$1"
+      shift
+    fi
+    "${COMPOSE[@]}" exec salt-master salt "$target" state.highstate "$@"
+    ;;
+  sync)
+    target="minion-*"
+    if [[ $# -gt 0 && "$1" != -* ]]; then
+      target="$1"
+      shift
+    fi
+    "${COMPOSE[@]}" exec salt-master salt "$target" saltutil.sync_all "$@"
+    ;;
+  pillar)
+    target="minion-*"
+    if [[ $# -gt 0 && "$1" != -* ]]; then
+      target="$1"
+      shift
+    fi
+    if [[ $# -gt 0 ]]; then
+      key="$1"
+      shift
+      "${COMPOSE[@]}" exec salt-master salt "$target" pillar.item "$key" "$@"
+    else
+      "${COMPOSE[@]}" exec salt-master salt "$target" pillar.items "$@"
+    fi
     ;;
   shell)
     if [[ $# -eq 0 ]]; then
